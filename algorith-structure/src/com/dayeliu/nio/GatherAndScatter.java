532ad1a271866154428cfa656b5149e97aba7d0f
andyliuzhong <liuchong89@126.com>
2020-11-15 22:17:59 +0800

    spring / springboot All

--- algorith-structure/src/com/dayeliu/nio/GatherAndScatter.java
+++ algorith-structure/src/com/dayeliu/nio/GatherAndScatter.java
@@ -4,15 +4,22 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.util.SortedMap;
 
 /**
  * @author liuch
  * @date 2020/11/12 - 22:28
  */
 public class GatherAndScatter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CharacterCodingException {
         gatherAndScatter();
+        enCodeAndDecode();
     }
 
     public static void gatherAndScatter(){
@@ -43,5 +50,34 @@ public class GatherAndScatter {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void enCodeAndDecode() throws CharacterCodingException {
+        SortedMap<String, Charset> map = Charset.availableCharsets();
+        map.entrySet().forEach(
+                entr -> System.out.println(entr.getKey() + ":" + entr.getValue())
+        );
+
+        //获取编解码器
+        Charset gbk = Charset.forName("GBK");
+        CharsetEncoder encoder = gbk.newEncoder();
+        CharsetDecoder decoder = gbk.newDecoder();
+
+        CharBuffer charBuffer = CharBuffer.allocate(1024);
+        charBuffer.put("编码测试");
+        charBuffer.flip();
+        //编码
+        ByteBuffer encodeByteBuffer = encoder.encode(charBuffer);
+        for (int i = 0; i < encodeByteBuffer.limit(); i++) {
+            System.out.println(encodeByteBuffer.get());
+        }
+
+
+        //解码
+        encodeByteBuffer.flip();
+        CharBuffer decodeCharBuffer = decoder.decode(encodeByteBuffer);
+        System.out.println("==================");
+        System.out.println(decodeCharBuffer.toString());
+
     }
 }
